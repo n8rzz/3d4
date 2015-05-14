ec095b754d0d9f88546dbd735f8a520e05f7d01e
Nate Geslin <teamtomkins23@gmail.com>
2015-05-14 00:09:36 -0500

    Adds static_pages controller, index view and stub rspec tests, Adds root route.

---
+++ spec/views/static_pages/index.html.erb_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe "static_pages/index.html.erb", type: :view do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
+
+# describe 'StaticPages' do
+
+#     subject { page }
+
+#     describe 'Home Page' do
+#         before { visit root_path }
+
+#         it { should have_selector('h1', text: 'Three D Four') }
+#     end
+# end
